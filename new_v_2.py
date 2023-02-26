e2b7c89914eb3f8312d36b5418018f1e3253f15a
oleg <didoleg@mail.ru>
2023-02-26 19:35:59 +0700

    add new_v_2

--- new_v_2.py
+++ new_v_2.py
@@ -21,7 +21,7 @@ def record_file(recording_time):
 	camera.wait_recording(recording_time)
 	camera.stop_recording
 
-	
+raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
 camera = picamera.PiCamera()
 recording_time = 10
 record_file(recording_time)
